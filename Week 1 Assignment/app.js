efeaf0d0796f8f24a22e99e298b2ea0a24a1e974
HashimZamer <zamerhashim@gmail.com>
2021-05-24 09:45:31 +0530

    Hashim

---
+++ Week 1 Assignment/app.js
@@ -0,0 +1,36 @@
+(function () {
+  'use strict';
+
+  angular.module('ItemCheck',[])
+
+  .controller('ItemCheckCtr', function ($scope) {
+
+    $scope.message = "";
+    $scope.dishes = "";
+    $scope.checkIfTooMuch = function () {
+      if($scope.dishes == "") {
+            $scope.message = "Please enter comma separated food items first :)";
+        } else {
+            var items = itemsInText($scope.dishes);
+            if(items == 0 || $scope.dishes == "") {
+                $scope.message = "Empty";
+            } else if(items <= 3) {
+                $scope.message = "Enjoy!";
+            } else {
+                $scope.message = "Too much!";
+            }
+        }
+
+    };
+
+
+    function itemsInText(string) {
+      var arrayOfStrings = string.split(",");
+      console.log(arrayOfStrings);
+      return arrayOfStrings.length;
+    }
+
+  });
+
+
+  })();
